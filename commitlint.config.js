798a59cea1d1eff43acbc59a5b04a5f524d6f897
Bijan Engineering <engineering@bijan.ir>
2026-07-12 14:17:31 -0500

    chore: update pnpm lockfile

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,16 +1,3 @@
-{
-  "commitlint": {
-    "extends": ["@commitlint/config-conventional"],
-    "rules": {
-      "type-enum": [2, "always", [
-        "feat", "fix", "chore", "docs", "style", "refactor", "perf",
-        "test", "build", "ci", "revert"
-      ]],
-      "scope-enum": [2, "always", [
-        "control-panel", "driver-app", "commercial", "ui", "types",
-        "config", "infra", "deps"
-      ]],
-      "subject-case": [0]
-    }
-  }
-}
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+};
